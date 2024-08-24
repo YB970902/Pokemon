ebf1e32f6d2380c843bb6e6a7c158f09d5ace3bf
YB970902 <78996787+YB970902@users.noreply.github.com>
2024-08-24 20:15:29 +0900

    12주차

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,47 +5,23 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    /// <summary> 현재 내가 서 있는 타일의 인덱스 </summary>
-    private Vector2Int tileIndex;
-    /// <summary> 이동할 다음 타일 인덱스 </summary>
-    private Vector2Int nextTileIndex;
-
-    /// <summary> 이동중인지 여부 </summary>
-    private bool isMove;
-
-    private float elaspedTime;
-    private const float durationTime = 0.5f;
-    
     private void Start()
     {
-        transform.position = Vector2.zero;
-        tileIndex = TileManager.Instance.GetTileIndex(transform.position);
-        isMove = false;
-        elaspedTime = 0.0f;
-    }
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.UpArrow) && isMove == false)
-        {
-            nextTileIndex = tileIndex;
-            ++nextTileIndex.y;
-            isMove = true;
-        }
-
-        if (isMove)
-        {
-            var tileManager = TileManager.Instance;
-
-            elaspedTime += Time.deltaTime;
-            var t = elaspedTime / durationTime;
-            transform.position = Vector2.Lerp(tileManager.GetPosition(tileIndex), tileManager.GetPosition(nextTileIndex), t);
-            if (t >= 1.0f)
-            {
-                isMove = false;
-                tileIndex = nextTileIndex;
-                elaspedTime = 0.0f;
-            }
-        }
+        TextAsset textAsset = Resources.Load<TextAsset>("MapData/MapData");
+        Debug.Log(textAsset.text);
+        string[] lines = textAsset.text.Split("\n");
+        var x = lines[0].Length;
+        var y = lines.Length;
+        Debug.Log($"x : {x}, y : {y}");
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Tile/Tile");
+        var spriteRenderer1 = new GameObject().AddComponent<SpriteRenderer>();
+        spriteRenderer1.sprite = sprites[0];
+        spriteRenderer1.transform.position = Vector2.zero;
+        var spriteRenderer2 = new GameObject().AddComponent<SpriteRenderer>();
+        spriteRenderer2.sprite = sprites[1];
+        spriteRenderer2.transform.position = Vector2.right;
+        var spriteRenderer3 = new GameObject().AddComponent<SpriteRenderer>();
+        spriteRenderer3.sprite = sprites[2];
+        spriteRenderer3.transform.position = Vector2.right * 2;
     }
 }
--- Assets/Scripts/Tile/TileManager.cs
+++ Assets/Scripts/Tile/TileManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 
-public class TileManager
+public class TileManager : MonoBehaviour
 {
     #region Singleton
+
     private static TileManager instance;
 
     public static TileManager Instance
@@ -14,28 +15,115 @@ public class TileManager
         {
             if (instance == null)
             {
-                instance = new TileManager();
+                instance = new GameObject().AddComponent<TileManager>();
             }
 
             return instance;
         }
     }
+
     #endregion
 
     /// <summary> 맵의 크기 </summary>
-    private Vector2Int mapSize = new Vector2Int(5, 5);
+    private Vector2Int mapSize = new Vector2Int();
+
+    private const string MapPath = "MapData/MapData";
+
+    private List<int> mapDatas;
+    private List<GameObject> mapObjects;
 
     /// <summary> 타일의 크기 </summary>
     private float tileSize = 1f;
 
-    public void Init()
+    private void Awake()
     {
+        mapDatas = new List<int>();
+        mapObjects = new List<GameObject>();
+        LoadMap(MapPath);
+    }
+
+    void LoadMap(string _mapPath)
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>(_mapPath);
+        if (textAsset == null)
+        {
+            Debug.LogError($"맵 데이터가 없습니다.{_mapPath}");
+        };
         
+        string[] lines = textAsset.text.Split("\n");
+        mapSize.x = lines[0].Length;
+        mapSize.y = lines.Length;
+
+        mapDatas.Clear();
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            var line = lines[i];
+            for (int j = 0 ; j < line.Length; ++j)
+            {
+                var tileID = int.Parse(line[j].ToString());
+                CreateTile(i * line.Length + j, tileID);
+                mapDatas.Add(tileID);
+            }
+        }
+    }
+
+    private void CreateTile(int _index, int _tileID)
+    {
+        Sprite[] tileSprites = Resources.LoadAll<Sprite>("Tile/Tile");
+        if (tileSprites == null)
+        {
+            Debug.LogError($"타일이 없습니다 tilePath : {tileSprites}");
+            return;
+        }
+        
+        string spriteName = string.Empty;
+        
+        switch (_tileID)
+        {
+            case 0:
+                spriteName = "Ground";
+                break;
+            case 1:
+                spriteName = "Tree";
+                break;
+            case 2:
+                spriteName = "Bush";
+                break;
+            default:
+                Debug.LogError($"타일이 없습니다 tileID : {_tileID}");
+                return;
+        }
+
+        Sprite sprite = null;
+        foreach (var tile in tileSprites)
+        {
+            if (tile.name == spriteName)
+            {
+                sprite = tile;
+                break;
+            }
+        }
+
+        if (sprite == null)
+        {
+            Debug.LogError($"스프라이트가 없음. spriteName : {spriteName}");
+            return;
+        }
+        
+        var spriteRenderer = new GameObject().AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+        spriteRenderer.transform.position = GetPosition(GetTileIndex(_index));
     }
 
     public Vector2 GetPosition(Vector2Int _tileIndex)
     {
         return new Vector2(_tileIndex.x * tileSize, _tileIndex.y * tileSize);
+    }
+
+    public Vector2Int GetTileIndex(int _index)
+    {
+        return new Vector2Int(_index % mapSize.x, mapSize.y - _index / mapSize.x);
     }
 
     public Vector2Int GetTileIndex(Vector2 _position)
